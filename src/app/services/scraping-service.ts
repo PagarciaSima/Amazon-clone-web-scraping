b15fc32df6f7fb4343c1f8b05e61d3af4ece0d27
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-07-29 17:31:12 +0200

    scraping service

---
+++ src/app/services/scraping-service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ScrapingService {
+
+    private apiUrl: string = 'https://scraper-api.decodo.com/v2/scrape';
+    private apiKey: string = 'API_KEY';
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    searchAmazon(query: string) {
+        const headers: HttpHeaders = new HttpHeaders({
+            'Accept': 'application/json',
+            'Authorization': `BASIC ${this.apiKey}`,
+            'Content-type': 'application/json'
+        });
+        const body = {
+            target: 'amazon_search',
+            query,
+            domain: 'com',
+            page_from: '1',
+            parse: true
+        }
+        return this.http.post<any>(this.apiUrl, body, { headers });
+    }
+}
